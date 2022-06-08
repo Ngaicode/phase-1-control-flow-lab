4e355c81fdb6205999d4e66e0e17dd944f60d33d
Ngaicode <mjombakevin376@gmail.com>
2022-06-08 21:05:47 +0300

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet) {
   // Write your code here!
+  if (feet <= 400) {
+    return "This one is on me!";
+  } else if (1999 < feet && feet < 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do.";
+  }
 }
+scuberGreetingForFeet(2000);
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city) {
   // Write your code here!
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
+ternaryCheckCity("NYC");
 
-function switchOnCharmFromTip(){
+// fun little use of the return keyword
+function switchOnCharmFromTip(tip) {
   // Write your code here!
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
 }
+switchOnCharmFromTip(" not as generous");
